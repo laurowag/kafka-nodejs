83ac93b3a85a00a5126d8f8284a375d49a6a6e6e
Lauro Wagnitz <lauro.wagnitz@agrotis.com>
2018-09-20 17:17:56 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,42 +1,64 @@
-import kafka from "kafka-node";
-import avro from "avsc"
- 
-const client = new kafka.Client("localhost:2181", "my-client-id", {
-    sessionTimeout: 300,
-    spinDelay: 100,
-    retries: 2
-});
- 
-const consumer = new kafka.Consumer(client, 
-    [
-        { topic: 'test', partition: 0 }
-    ],
-    {
-        autoCommit: false,
-        encoding:'buffer'
-    }
-);
+/*
+ * node-rdkafka - Node.js wrapper for RdKafka C/C++ library
+ *
+ * Copyright (c) 2016 Blizzard Entertainment
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE.txt file for details.
+ */
+var avro = require("avsc");
+
+var Transform = require('stream').Transform;
+
+var Kafka = require('node-rdkafka');
 
 const type = avro.Type.forSchema({
     name: 'Cliente',
     type: 'record',
     fields: [
-      {name: 'codigo', type: ['null', 'long'] },
+      {name: 'id', type: 'int' },
       {name: 'nome', type: ['null', 'string'] }
     ]
   });
 
-consumer.on('message', function (message) {
-    try {
-        //var buf = new Buffer(message.value, 'binary'); // Read string into a buffer.
-        var decodedMessage = type.fromBuffer(message.value); // Skip prefix.
-        console.log(decodedMessage);
-    } catch (error) {
-        console.log("error: "+error.message);
-    }
+var consumer = new Kafka.KafkaConsumer({
+  'metadata.broker.list': '10.2.157.115:9092',
+  'group.id': 'librd-test',
+  'socket.keepalive.enable': true,
+  'enable.auto.commit': true,
+  //'security.protocol':'SASL_PLAINTEXT',
+  //'sasl.mechanism':'PLAIN',
+  //'sasl.username':'admin',
+  //'sasl.password':'admin-password',
+  });
+  
+var topicName = 'laurowag';
+
+var counter = 0;
+var numMessages = 5;
+
+consumer.on('ready', function(arg) {
+    console.log('consumer ready.' + JSON.stringify(arg));
+  
+    consumer.subscribe([topicName]);
+    //start consuming messages
+    consumer.consume();
+  });
+
+consumer.on('data', function(m) {
+    var decodedMessage = type.decode(m.value); // Skip prefix.
+    console.log(decodedMessage);
 });
- 
-// For this demo we just log producer errors to the console.
-consumer.on("error", function(error) {
-    console.error(error);
-});
+
+consumer.on('error', function(err) {
+    console.error('Error from consumer');
+    console.error(err);
+  });
+
+//starting the consumer
+consumer.connect();
+
+//stopping this example after 30s
+setTimeout(function() {
+  consumer.disconnect();
+}, 60000);
